eba4039c7d5a0c45f2269758da958fdd42021064
psychokot11 <z.stawarska@gmail.com>
2022-01-31 22:25:16 +0100

    Added pac-man to the game

--- src/index.js
+++ src/index.js
@@ -39,10 +39,38 @@ document.addEventListener("DOMContentLoaded", () => {
     1, 1, 1, 1, 1, 1, 1, 1, 1,
   ];
 
+  const squares = [];
+
   //Legend
   //0 - pac-dot
   //1 - wall
   //2 -  ghost-lair
   //3 - power-pellet
   //4 - empty
+
+  //draw the grid and render it
+
+  function createBoard() {
+    for (let i = 0; i < layout.length; i++) {
+      const square = document.createElement("div");
+      grid.appendChild(square);
+      squares.push(square);
+
+      //add layout to the board
+      if (layout[i] === 0) {
+        squares[i].classList.add("pac-dot");
+      } else if (layout[i] === 1) {
+        squares[i].classList.add("wall");
+      } else if (layout[i] === 3) {
+        squares[i].classList.add("power-pellet");
+      }
+    }
+  }
+
+  createBoard();
+
+  //strting position of pac-man
+  let pacmanCurrentIndex = 490;
+
+  squares[pacmanCurrentIndex].classList.add("pac-man");
 });
